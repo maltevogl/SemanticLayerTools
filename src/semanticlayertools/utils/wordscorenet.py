f1c4c41e4346ec1c7516d4071948ed5b5a01794b
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2021-10-01 13:52:30 +0200

    wip updt origin, data testing complete, no tests written for clustering yet

--- src/semanticlayertools/utils/wordscorenet.py
+++ src/semanticlayertools/utils/wordscorenet.py
@@ -13,6 +13,7 @@ def run(
     tempFiles=True,
     outPath='./',
     textColumn='text',
+    yearColumn='year',
     authorColumn='author',
     pubIDColumn='publicationID',
     scoreLimit=1.0
@@ -30,17 +31,20 @@ def run(
     links = LinksOverTime(
         dataframe,
         authorColumn=authorColumn,
-        pubIDColumn=pubIDColumn
+        pubIDColumn=pubIDColumn,
+        yearColumn=yearColumn
     )
     clusters = Clustering()
     if tempFiles is True:
         basedir = tempfile.TemporaryDirectory().name
+        clusterout = outPath
     else:
         timestamp = datetime.now().strftime("_%Y_%m_%d")
-        basedir = outPath + timestamp
+        basedir = outPath + 'Clustering' + timestamp
+        clusterout = f'{basedir}/clusters/'
     for subdir in ['scores', 'links', 'clusters']:
         os.makedirs(os.path.join(basedir, subdir))
-    sc, outDict = score.run(
+    score.run(
         write=True, outpath=f'{basedir}/scores/', recreate=True
     )
     links.run(
@@ -51,5 +55,23 @@ def run(
     )
     clusters.run(
         pajekPath=f'{basedir}/links/',
-        outPath=f'{outPath}',
+        outPath=clusterout,
     )
+    with open(f'{basedir}/README.txt', 'w+') as file:
+        file.write(
+            f"""Run of clustering {datetime.now().strftime("%Y_%m_%d")}
+
+            Text cleaned in column: {textColumn} (html tags removed and lemmatized)
+            Authors information from column: {authorColumn}
+            Unique publication IDs from columns: {pubIDColumn}
+            Ngram scores greater {scoreLimit} were considered for link creation.
+            Clustering result in folder: {clusterout}
+            """
+        )
+        if tempFiles is True:
+            file.write(
+                'Temporay files for wordscores and multilayer network were deleted.'
+            )
+    print(f"""Results in {clusterout}.\n
+    Head over to https://www.mapequation.org/alluvial/ to visualize the ftree files.
+        """)
