b481ef905e183c4f2e332b987458c07c92bb50f4
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2021-12-16 14:46:28 +0100

    finish streamgraph, add routine for reports, WIP

---
+++ src/semanticlayertools/clustering/reports.py
@@ -0,0 +1,169 @@
+import re
+import os
+from tqdm import tqdm
+
+import spacy
+import textacy
+import textacy.tm
+import pandas as pd
+import multiprocessing
+
+num_processes = multiprocessing.cpu_count()
+
+mainLanguageCorp = 'en_core_web_lg'
+nlp = spacy.load(mainLanguageCorp)
+
+
+class ClusterReports():
+
+    def __init__(
+        self, infile:str, metadatapath:str, outpath:str, iteration: int,
+        numberProc: int=num_processes, minClusterSize: int=1000
+    ):
+        self.iteration = iteration
+        self.numberProc = numberProc
+        self.minClusterSize = minClusterSize
+        self.metadatapath = metadatapath
+        self.outpath = outpath
+
+        self.clusterdf = pd.read_csv(infile)
+        basedata = self.clusterdf.groupby(['year', 'cluster']).size().to_frame('counts').reset_index()
+        largeClusterList = list(basedata.groupby('cluster').sum().query(f'counts > {self.minClusterSize}').index)
+
+        self.clusternodes = self.clusterdf.query(
+            'cluster in @largeClusterList'
+        )
+
+    def create_corpus(self, dataframe):
+        """Create corpus out of dataframe."""
+        docs = []
+        titles = [x[0] for x in dataframe.title.values if type(x) == list]
+        for title in tqdm(titles):
+            try:
+                # text pre-processing
+                title = re.sub("\n", " ", title)
+                title = re.sub("[\r|\t|\x0c|\d+]", "", title)
+                title = re.sub("[.,]", "", title)
+                title = re.sub("\\\'s", "'s", title)
+                title = title.lower()
+
+                doc = nlp(title)
+
+                tokens_without_sw = ' '.join([t.lemma_ for t in doc if not t.is_stop])
+
+                docs.append(tokens_without_sw)
+            except:
+                print(title)
+                raise
+
+        corpus_titles = textacy.Corpus(mainLanguageCorp, data=docs)
+        return corpus_titles
+
+
+    def find_topics(self, corpus_titles: list, n_topics: int, top_words: int, outpath:str='./', writeReport: bool=False):
+        """Calculate topics in corpus."""
+        vectorizer = textacy.representations.vectorizers.Vectorizer(
+            tf_type="linear",
+            idf_type="smooth",
+            norm="l2",
+            min_df=2,
+            max_df=0.95
+        )
+        tokenized_docs = (
+            (
+                term.lemma_ for term in textacy.extract.terms(doc, ngs=1, ents=True)
+            ) for doc in corpus_titles
+        )
+        doc_term_matrix = vectorizer.fit_transform(tokenized_docs)
+
+        model = textacy.tm.TopicModel("nmf", n_topics)
+        model.fit(doc_term_matrix)
+
+        doc_topic_matrix = model.transform(doc_term_matrix)
+
+        topics = []
+        for topic_idx, top_terms in model.top_topic_terms(vectorizer.id_to_term, top_n=top_words):
+            topics.append("topic " + str(topic_idx) + ": " + "   ".join(top_terms))
+        if writeReport is True:
+            outfile.write(f'\n\n\tTopics in cluster for {n_topics} topics:\n')
+            for topic in topics:
+                outfile.write(f'\t\t{topic}\n')
+        else:
+            print("\nTopics in the cluster:\n")
+            return topics
+
+    def fullReport(self, cluster):
+        """Generate full cluster report."""
+        with open(f'{outFolderReports}Report_{cluster}.txt', 'a') as outfile:
+            selection = self.clusterdf.query('cluster == @cluster')
+            nodeList = list(set(selection.node.values))
+            starttime = time.time()
+            result = {}
+            resultMeta = []
+            for key, vals in groupby(sorted(nodeList), lambda x: x[:4]):
+                result[int(key)] = list(vals)
+
+            for key in result.keys():
+                if key > 1949 and key < 2005 and key != 1996:
+                    yeardata = pd.read_json(f'{inFolderMetadata}{key}_meta.json', lines=True)
+                    selectNodedata = yeardata[yeardata.nodeID.isin(result[key])]
+                    resultMeta.append(selectNodedata)
+
+            metadata = pd.concat(resultMeta)
+            metadata.to_json(
+                f'{outFolderReports}meta/cluster_{cluster}_meta.json',
+                orient='records',
+                lines=True
+            )
+            foundNodes = [x[0] for x in metadata.bibcode.values]
+            notFound = [x for x in nodeList if x not in foundNodes]
+
+            outfile.write(
+                f'\tGot {len(nodeList)} unique publications in time range:\
+                 {selection.year.min()} to {selection.year.max()}.\n'
+            )
+            outfile.write(
+                f'\t\tFound metadata for {metadata.shape[0]} publications.\n'
+            )
+            outfile.write(
+                f'\t\tThere are {len([x for x in foundNodes if x not in nodeList])}\
+                found publications which where NOT in the query list.\n'
+            )
+            outfile.write(
+                f'\t\tThere are {len(notFound)} publication(s) which where NOT found:\n'
+            )
+
+            topAuthors = Counter(
+                [x for y in [x for x in metadata.author.values if type(x) == list] for x in y]
+            ).most_common(20)
+            outfile.write('\n\tThe top authors of this cluster are:\n')
+            for elem in topAuthors:
+                outfile.write(f'\t\t{elem[0]}: {elem[1]} pubs\n.')
+            topAffils = Counter(
+                [x for y in [x for x in metadata.aff.values if type(x) == list] for x in y]
+            ).most_common(20)
+            outfile.write('\n\tThe top 20 affiliations of this cluster are:\n')
+            for elem in topAffils:
+                outfile.write(f'\t\t{elem[0]}: {elem[1]} authors.\n')
+            outfile.write(
+                f'\n\n\tFinished analysis of cluster {cluster} with {len(nodeList)}\
+                 unique publications in {time.time()- starttime} seconds.\n\n'
+            )
+            corpus = create_corpus(metadata)
+            find_topics(
+                corpus, n_topics=15, top_words=10, writeReport=True, outfile=outfile
+            )
+            find_topics(
+                corpus, n_topics=50, top_words=10, writeReport=True, outfile=outfile
+            )
+            outfile.write(
+                f'\n\n\tFinished analysis of topics in {cluster} in {time.time()- starttime} seconds.\n\n'
+            )
+            return cluster
+
+    def processClusters(self, publicationIDcolumn: str='nodeID'):
+        for filename in tqdm(os.listdir(self.metadatapath)):
+            filepath = os.path.join(self.metadatapath, filename)
+            data = pd.read_json(filepath, lines=True)
+            selectMerge = data.merge(self.clusternodes, left_on=publicationIDcolumn, right_on='node', how='inner')
+            selectMerge.to_json(os.path.join(self.outpath, 'merge_' + filename) , orient='records', lines=True)
--- src/semanticlayertools/visual/utils.py
+++ src/semanticlayertools/visual/utils.py
@@ -1,38 +1,76 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from scipy import stats
+from typing import TypeVar
+
+smoothing = TypeVar('smoothing', bool, float)
 
 
-def streamgraph(filepath):
-    """Plot streamgraph of cluster sizes vs years."""
+def gaussian_smooth(x, y, grid, sd):
+    weights = np.transpose([stats.norm.pdf(grid, m, sd) for m in x])
+    weights = weights / weights.sum(0)
+    return (weights * y).sum(1)
+
+
+def streamgraph(filepath: str, smooth: smoothing=False, minClusterSize: int=1000, showNthGrid: int=5):
+    """Plot streamgraph of cluster sizes vs years.
+
+    Based on https://www.python-graph-gallery.com/streamchart-basic-matplotlib
+    """
     basedf = pd.read_csv(filepath)
     basedata = basedf.groupby(['year', 'cluster']).size().to_frame('counts').reset_index()
     yearbase = [
-        str(x) for x in range(
+        x for x in range(
             int(basedata.year.min()), int(basedata.year.max()) + 1
         )
     ]
+    largeclu = list(basedata.groupby('cluster').sum().query(f'counts > {minClusterSize}').index)
     cluDict = {}
     for clu in basedata.cluster.unique():
-        cluvec = []
-        basedf = basedata.query('cluster == @clu')
-        baseyears = basedf.year.unique()
-        for year in yearbase:
-            if year in baseyears:
-                cluvec.append(basedf.query('year == @year').counts.iloc[0])
-            else:
-                cluvec.append(0)
-        cluDict[clu] = cluvec
+        if clu in largeclu:
+            cluvec = []
+            basedf = basedata.query('cluster == @clu')
+            baseyears = list(basedf.year.unique())
+            for year in yearbase:
+                if year in baseyears:
+                    cluvec.append(basedf.query('year == @year').counts.iloc[0])
+                else:
+                    cluvec.append(0)
+            cluDict[clu] = cluvec
 
-    fig, ax = plt.subplots(figsize=(10, 7))
-    ax.stackplot(
-        yearbase,
-        cluDict.values(),
-        labels=cluDict.keys(),
-        baseline='sym'
-    )
+    fig, ax = plt.subplots(figsize=(16, 9))
+    if type(smooth) is float:
+        grid = np.linspace(yearbase[0], yearbase[-1], num=100)
+        y = [np.array(x) for x in cluDict.values()]
+        y_smoothed = [gaussian_smooth(yearbase, y_, grid, smooth) for y_ in y]
+        ax.stackplot(
+            grid,
+            y_smoothed,
+            labels=cluDict.keys(),
+            baseline="sym"
+            ,colors=plt.get_cmap('tab20').colors
+        )
+
+        pass
+    else:
+        ax.stackplot(
+            yearbase,
+            cluDict.values(),
+            labels=cluDict.keys(),
+            baseline='sym',
+            colors=plt.get_cmap('tab20').colors
+        )
+    ax.legend()
     ax.set_title('Cluster sizes')
     ax.set_xlabel('Year')
     ax.set_ylabel('Number of publications')
-    ax.axhline(0, color="black", ls="--")
-
+    ax.yaxis.set_ticklabels([])
+    ax.xaxis.grid(color='gray')
+    temp = ax.xaxis.get_ticklabels()
+    temp = list(set(temp) - set(temp[::showNthGrid]))
+    for label in temp:
+        label.set_visible(False)
+    ax.set_axisbelow(True)
+    #plt.show()
     return fig
